335009b446a7eacfaa46f0a315250ad05ff8c191
Aza Maulana <azamaulanaaa@gmail.com>
2026-02-10 14:37:50 +0700

    feat: add vite pwa

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,21 @@
+/// <reference types="vite-plugin-pwa/client" />
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 
 import { App } from "@/app.tsx";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload to update?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("App is ready to work offline!");
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,23 @@ import deno from "@deno/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { comlink } from "vite-plugin-comlink";
+import { VitePWA } from "vite-plugin-pwa";
 
 const __dirname = dirname(fromFileUrl(import.meta.url));
 
 export default defineConfig({
-  plugins: [deno(), react(), tailwindcss(), comlink()],
+  plugins: [
+    deno(),
+    react(),
+    tailwindcss(),
+    comlink(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
   worker: {
     plugins: () => [comlink()],
   },
