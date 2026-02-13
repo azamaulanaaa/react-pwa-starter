667c02b9107449ead6914d19806ded67f387bd7d
Aza Maulana <azamaulanaaa@gmail.com>
2026-02-13 07:46:44 +0700

    feat: add vite tsconfig path plugin

--- .ladle/vite.config.ts
+++ .ladle/vite.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-const SRC_PATH = new URL("../src", import.meta.url).pathname;
-
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     react(),
     tailwindcss(),
   ],
-  resolve: {
-    alias: { "@": SRC_PATH },
-  },
 });
