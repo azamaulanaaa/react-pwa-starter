1cf97f659f85bf0de1d6d5d878cdadc7945110c1
Aza Maulana <azamaulanaaa@gmail.com>
2026-06-23 17:09:57 +0700

    feat!: use todo list for index page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,44 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-import { useWorker } from "@/components/worker_context.tsx";
-import { Greeting, GreetingProps } from "@/components/form/greeting/index.tsx";
+import { FormTask, FormTaskProps } from "@/components/form/task/index.tsx";
+import { ListTask, type Task } from "@/components/list/task/index.tsx";
 
-export function Index() {
-  const worker = useWorker();
+function Index() {
+  const [data, setData] = useState<Task[]>([]);
 
   useEffect(() => {
     document.title = "Home";
   }, []);
 
-  const handleOnSubmit: GreetingProps["onSubmit"] = async (value) => {
-    const message = await worker.greeting.getGreeting(value.name);
-    alert(message);
+  const handleOnSubmit: FormTaskProps["onSubmit"] = (value) => {
+    setData((data) => [...data, {
+      id: data.length.toString(),
+      description: value.task,
+      isDone: false,
+    }]);
   };
 
-  return <Greeting onSubmit={handleOnSubmit} />;
+  const handleOnToggleDone = (id: string, isDone: boolean) => {
+    setData((data) =>
+      data.map((item) => item.id == id ? { ...item, isDone } : item)
+    );
+  };
+
+  const handleOnDelete = (id: string) => {
+    setData((data) => data.filter((item) => item.id != id));
+  };
+
+  return (
+    <div className="flex flex-col gap-8">
+      <FormTask onSubmit={handleOnSubmit} />
+      <ListTask
+        data={data}
+        onToggleDone={handleOnToggleDone}
+        onDelete={handleOnDelete}
+      />
+    </div>
+  );
 }
 
 export const Route = createFileRoute("/")({
