a512535688eccd17e740ef74f4a2a42059cdaac8
Aza Maulana <azamaulanaaa@gmail.com>
2026-06-23 17:10:14 +0700

    chore: remove greeting example

--- src/worker/main.ts
+++ src/worker/main.ts
@@ -5,6 +5,3 @@ function markAsProxy<T>(lib: T): T & ProxyMarked {
 }
 
 export { setLanguage } from "./i18n.ts";
-
-import * as greetingRaw from "./greeting.ts";
-export const greeting = markAsProxy(greetingRaw);
