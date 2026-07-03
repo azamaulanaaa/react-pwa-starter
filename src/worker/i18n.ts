4f311984ac364ee0a3286002a5f9586cb1fac7a8
Aza Maulana <azamaulanaaa@gmail.com>
2026-07-03 09:15:18 +0700

    refactor!: use effect on i18n worker

--- src/worker/i18n.ts
+++ src/worker/i18n.ts
@@ -1,19 +1,25 @@
+import { Effect } from "effect";
+
 import { i18nbase } from "@/lib/i18n.ts";
 
-const i18nPromise = i18nbase("worker");
+const makeI18nCache = Effect.cached(Effect.promise(() => i18nbase("worker")));
 
-export async function getI18n() {
-  return await i18nPromise;
+const getI18n = Effect.gen(function* () {
+  return yield* makeI18nCache;
+}).pipe(Effect.flatten);
+
+export function changeLanguage(lang: string) {
+  return Effect.gen(function* () {
+    const i18n = yield* getI18n;
+    yield* Effect.promise(() => i18n.changeLanguage(lang));
+  });
 }
 
-export async function changeLanguage(lang: string) {
-  const i18n = await getI18n();
-  await i18n.changeLanguage(lang);
-}
+export function getT(ns: string) {
+  return Effect.gen(function* () {
+    const i18n = yield* getI18n;
+    const fixedT = i18n.getFixedT(ns);
 
-export async function getT(ns: string) {
-  const i18n = await getI18n();
-  const fixedT = i18n.getFixedT(ns);
-
-  return { t: fixedT, i18n };
+    return { t: fixedT, i18n };
+  });
 }
