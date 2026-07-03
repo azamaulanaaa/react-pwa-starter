49a80c4e60f7b73c7c779b215760ecb5a16eac51
Aza Maulana <azamaulanaaa@gmail.com>
2026-07-03 10:18:01 +0700

    feat: add effect handler for comlink

--- src/components/worker_context.tsx
+++ src/components/worker_context.tsx
@@ -1,15 +1,18 @@
 import { createContext, ReactNode, useContext } from "react";
+import { Effect } from "effect";
 
 import "@/lib/worker.ts";
 export type WorkerType = typeof import("@/worker/main.ts");
 
 export type SyncRemoteProxy<T> = {
   [K in keyof T]: T[K] extends (...args: infer Args) => infer Ret
-    ? (...args: Args) => Promise<Awaited<Ret>>
+    ? (
+      ...args: Args
+    ) => Ret extends Effect.Effect<infer Success, any, any> ? Promise<Success> // 1. If it's an Effect function
+      : Promise<Awaited<Ret>> // 2. If it's a regular sync or async function
     : T[K] extends object ? SyncRemoteProxy<T[K]>
     : Promise<T[K]>;
 };
-
 const WorkerContext = createContext<null | SyncRemoteProxy<WorkerType>>(null);
 
 const worker = new ComlinkWorker(
--- src/lib/worker.ts
+++ src/lib/worker.ts
@@ -1,4 +1,5 @@
 import * as Comlink from "comlink";
+import { Effect } from "effect";
 
 if (!Comlink.transferHandlers.has("AUTO_FUNCTION_PROXY")) {
   Comlink.transferHandlers.set("AUTO_FUNCTION_PROXY", {
@@ -10,6 +11,23 @@ if (!Comlink.transferHandlers.has("AUTO_FUNCTION_PROXY")) {
     },
     deserialize: (port: MessagePort) => {
       return Comlink.wrap(port);
+    },
+  });
+}
+
+if (!Comlink.transferHandlers.has("EFFECT_PROXY")) {
+  Comlink.transferHandlers.set("EFFECT_PROXY", {
+    canHandle: (val): val is Effect.Effect<any, any, never> =>
+      Effect.isEffect(val),
+    serialize: (effectInstance: Effect.Effect<any, any, never>) => {
+      const { port1, port2 } = new MessageChannel();
+      const executionPromise = () => Effect.runPromise(effectInstance);
+      Comlink.expose(executionPromise, port1);
+      return [port2, [port2]];
+    },
+    deserialize: (port: MessagePort) => {
+      const proxyFunc = Comlink.wrap<() => Promise<any>>(port);
+      return proxyFunc();
     },
   });
 }
