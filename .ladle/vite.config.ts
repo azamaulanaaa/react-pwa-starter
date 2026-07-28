a5f6371b0a48b303218309bc551797b43a87f990
Aza Maulana <azamaulanaaa@gmail.com>
2026-07-29 06:56:01 +0700

    fix: ladle vite config cannot resolve tsconfig path

--- .ladle/vite.config.ts
+++ .ladle/vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from "vite";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
   resolve: {
-    tsconfigPaths: true,
+    alias: {
+      "~": resolve(__dirname, ".."),
+      "@": resolve(__dirname, "../src"),
+    },
   },
 });
