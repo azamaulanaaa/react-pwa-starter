3e780ce955a839f81e707c9b99884894c289359c
Aza Maulana <azamaulanaaa@gmail.com>
2026-08-24 18:17:17 +0700

    feat: add Prettify helper type

--- src/global.d.ts
+++ src/global.d.ts
@@ -9,3 +9,11 @@ declare module "@tanstack/react-router" {
     breadcrumb?: string | ((match: any) => string);
   }
 }
+
+declare global {
+  type Prettify<T> =
+    & {
+      [K in keyof T]: T[K];
+    }
+    & {};
+}
